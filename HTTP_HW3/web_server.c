73724cca6bd5332ffc329a8bbe1abd1334883d78
Lfollow-CS <lcs0713v@gmail.com>
2021-11-25 22:55:20 +0900

    HTTP_HW3

--- HTTP_HW3/web_server.c
+++ HTTP_HW3/web_server.c
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]){
 			char fbuf[BUF_SIZE];
 			int n;
 			if((fd=open(pwd,O_RDONLY))==-1){
-				char* notfound_content[100] = "<h1>404 Not Found</h1>";
+				char notfound_content[100] = "<h1>404 Not Found</h1>";
 				write(clnt_sock, notfound, sizeof(notfound)-1);
 				write(clnt_sock, notfound_content, sizeof(notfound_content)-1);
 				printf("Not Found\n");
